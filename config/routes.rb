fdedd8c399681da7e7c5fe985e5a0c9eaf95d936
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-11-26 19:11:54 +0000

    Fixed User#Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "users", :action => "index" })
-  get("/users", { :controller => "users", :action => "all_users" })
-  get("/users/:username", { :controller => "user", :action => "show" })
-  post("/insert_user_record", { :controller => "venues", :action => "create" })
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:username", { :controller => "users", :action => "show" })
+  get("/insert_user_record", { :controller => "users", :action => "create" })
   post("/update_users/:user_id", { :controller => "users", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
