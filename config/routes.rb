aa02bb45fb588569ae18fc4cb062f57ab8d5cb1d
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-11-26 19:36:00 +0000

    Fixed venue details page

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   post("/update_users/:user_id", { :controller => "users", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
-  post("/insert_venue_record", { :controller => "venues", :action => "create" })
-  get("/venue/:an_id", { :controller => "venue", :action => "show" })
+  get("/insert_venue_record", { :controller => "venues", :action => "create" })
+  get("/venues/:an_id", { :controller => "venues", :action => "show" })
   post("/update_venue/:the_id", { :controller => "application", :action => "update" })
-  get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
+  get("/delete_venue/:id_to_delete", { :controller => "venues", :action => "destroy" })
   
   post("/insert_comment_record", { :controller => "comments", :action => "create" })
 end
